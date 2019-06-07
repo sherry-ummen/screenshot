37561d67451cb86dd8411a445f945c920cbe7594
Sherry Ummen <sherry.ummen@gmail.com>
2019-06-07 20:02:43 +0300

    System tray icon and context menu

--- Screenshot/App.xaml.cs
+++ Screenshot/App.xaml.cs
@@ -1,17 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace Screenshot
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        private NotifyIcon _systemTrayIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            CreateSystemTrayIcon();
+        }
+
+        private void CreateSystemTrayIcon()
+        {
+            InitializeSystemTrayIcon();
+            CreateContextMenuForSystemTrayIcon();
+        }
+
+        private void CreateContextMenuForSystemTrayIcon()
+        {
+            _systemTrayIcon.ContextMenuStrip =
+             new System.Windows.Forms.ContextMenuStrip();
+            _systemTrayIcon.ContextMenuStrip.Items.Add("Shortcut Keys").Click += ShowWindow;
+            _systemTrayIcon.ContextMenuStrip.Items.Add("Exit").Click += ExitApp;
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            MainWindow.Close();
+            _systemTrayIcon.Dispose();
+        }
+
+        private void InitializeSystemTrayIcon()
+        {
+            _systemTrayIcon = new NotifyIcon();
+            _systemTrayIcon.DoubleClick += ShowWindow;
+            _systemTrayIcon.Icon = Screenshot.Properties.Resources.screenshot;
+            _systemTrayIcon.Visible = true;
+        }
+
+        private void ShowWindow(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
