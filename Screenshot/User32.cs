3f782ed4c2f2801d47c3745a0dc0a58af0bd4634
Sherry Ummen <sherry.ummen@gmail.com>
2019-06-08 08:10:02 +0300

    Cleanup

--- Screenshot/User32.cs
+++ Screenshot/User32.cs
@@ -21,15 +21,6 @@ namespace Screenshot
         }
 
         [DllImport("user32.dll")]
-        public static extern IntPtr GetDesktopWindow();
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetWindowDC(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
-
-        [DllImport("user32.dll")]
         public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
 
         [DllImport("user32.dll")]
